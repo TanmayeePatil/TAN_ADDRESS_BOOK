ecc8b73ff0598cd73d26a1f4a3e7e6a0a81a1f2f
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-11 08:10:08 +0530

    first commit

---
+++ src/addressbookmngt_tan/Person.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package addressbookmngt_tan;
+
+/**
+ *
+ * @author tanmayee
+ */
+public class Person {
+
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	String zip;
+	String phoneNumber;
+	
+	public Person(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public JSONObject toJsonObject() {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("First Name", firstName);
+		jsonObject.put("Last Name", lastName);
+		jsonObject.put("Address", address);
+		jsonObject.put("City", city);
+		jsonObject.put("State", state);
+		jsonObject.put("Zip", zip);
+		jsonObject.put("Phone Number", phoneNumber);
+		return jsonObject;
+	}
+}
